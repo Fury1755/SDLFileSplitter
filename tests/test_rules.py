7cfb5b4aaa20e0910f6c220efb2bbfc1189042d6
Fury1755 <robbstark1755@gmail.com>
2026-07-14 10:54:19 +0800

    test(test_rules.py): added basic tests for class 'Metadata'

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -5,6 +5,7 @@ Tests text parsing rules.
 import pytest
 
 from src.file_splitter.text_parser import rules
+from dataclasses import fields
 
 
 @pytest.mark.parametrize(
@@ -35,6 +36,7 @@ def test_get_name(text: str, expected_name: str | None):
     [
         ("Page 2103902190 of 2", False),
         ("page 3 of 3", False),
+        ("", False),
         ("Page 10 of 10", True),
         ("Page 9 of 9", True),
     ],
@@ -45,3 +47,26 @@ def test_is_final_page(text: str, is_final: bool):
     """
 
     assert is_final == rules.is_final_page(text)
+
+
+def test_metadata_defaults():
+    """
+    Quick sanity check.
+    Tests whether the default values of the Metadata class are as expected.
+    """
+
+    example = rules.Metadata()
+
+    assert example.name is None
+    assert example.is_final_page is False
+
+
+def test_metadata_field_count():
+    """
+    Tests whether the properties of metadata are accounted for in testing.
+    Will fail if someone forgets to update the metadata tests (including this one) when
+    adding a new property to the dataclass.
+    """
+
+    example = rules.Metadata()
+    assert len(fields(example)) == 2
