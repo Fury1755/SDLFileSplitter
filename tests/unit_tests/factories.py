2e730906913a7c1dab6db2a061c4a6b26812f7be
Fury1755 <robbstark1755@gmail.com>
2026-07-26 13:46:37 +0800

    tests(factory): added a factory module to construct test objects. Currently only serves PDFInstance

---
+++ tests/unit_tests/factories.py
@@ -0,0 +1,67 @@
+"""
+Utility module for creating test objects.
+"""
+
+from unittest.mock import MagicMock
+from pymupdf import Document, Page
+from file_splitter.pdf_instance import PDFInstance
+
+
+def create_page_list(text: list[str], doc: Document) -> list[Page]:
+    """
+    Creates a list of pymupdf.Page objects with text inside it.
+
+    Args:
+        text(list[str]): A list of strings. The number of pages correspond to the number of
+                        strings in the list.
+        doc(pymupdf.Document): A document passed externally as a dependency. Used so that
+                                the individual pages don't lose the value of their page.number
+                                attributes.
+
+    Returns:
+        A list containing pymupdf.Page objects. The index of each page contains text
+        corresponding to the index of the strings in the list.
+    """
+
+    page_list: list[Page] = []
+    for page_text in text:
+        doc.new_page()
+
+    for i, page_text in enumerate(text):
+        doc[i].insert_text((50, 50), page_text)
+        page_list.append(doc[i])
+        print(page_list[i].number)
+    return page_list
+
+
+class TestablePDFInstance(PDFInstance):  # this is inheritance, not composition
+    """
+    Wrapper around PDFInstance. Has private methods that manually set internal state
+    of PDFInstance for testing.
+    """
+
+    # stops pytest from looking for tests in this class, which will raise a warning
+    __test__ = False
+
+    def __init__(self, **kwargs):
+        """
+        Initializes with default values for testing purposes
+        """
+        self._ocr_engine = MagicMock()
+        self._runtime_parent_dir = "blah"
+        self._page_buffer = []
+        self._current_name = None
+        self._output_folder = "blah blah"
+
+    def set_flush_state(self, page_list: list[Page], name: str):
+        """
+        Sets its state to be flushed
+        """
+        self._page_buffer = page_list
+        self._current_name = name
+
+    def test_flush(self):
+        """
+        Expose the private _.flush method so testing doesn't violate encapsulation.
+        """
+        self._flush()
