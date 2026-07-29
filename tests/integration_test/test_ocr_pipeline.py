f096d0aaecae41f3331cae56203ea55d62ca2e8b
Fury1755 <robbstark1755@gmail.com>
2026-07-29 18:36:33 +0800

    tests(integration): parametrized the integration test for the happy path

--- tests/integration_test/test_ocr_pipeline.py
+++ tests/integration_test/test_ocr_pipeline.py
@@ -3,6 +3,7 @@ This module contains an integration test for the ocr pipeline.
 Uses the real ocr engine.
 """
 
+import pytest  # pyright: ignore[reportMissingImports]
 from pymupdf import Document
 
 from file_splitter.os_utils import create_folder
@@ -12,16 +13,34 @@ from src.config import TESS_DATA_PATH
 from tests.factories import create_test_document
 
 
-def test_tesseract_pipeline(tmp_path) -> None:
+@pytest.mark.parametrize(
+    "page_texts, expected_names",
+    [
+        (
+            [
+                "Shadow Pte Ltd",
+                "Page 5 of 5",
+                "Bluewind Pte Ltd",
+                "Page 2 of 3",
+                "Page 3 of 3",
+            ],
+            ["Shadow Pte Ltd", "Bluewind Pte Ltd"],
+        )
+    ],
+)
+def test_tesseract_pipeline_success(
+    tmp_path, page_texts: list[str], expected_names: list[str]
+) -> None:
     """
     Tests the tesseract pipeline. Verifies that tesseract is working as expected.
     """
 
-    page_texts: list[str] = ["Shadow Pte Ltd", "Page 5 of 5"]
     doc: Document = create_test_document(text=page_texts)
     tesser_engine: TesseractEngine = TesseractEngine(tess_data_path=TESS_DATA_PATH)
     test_instance: PDFInstance = PDFInstance(tesser_engine, tmp_path, create_folder)
     test_instance.split_statements(doc)
     split_files = list(tmp_path.glob("**/*.pdf"))
-    file_names = [file.name for file in split_files]
-    assert "Shadow Pte Ltd" in file_names[0]
+    split_names = [file.name for file in split_files]
+    for name in expected_names:
+        assert any(name in filename for filename in split_names)
+        # returns True as long as at least one name is in split_names
