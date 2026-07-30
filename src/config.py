add992c976b396818a132bc7358c73599570cfae
Fury1755 <robbstark1755@gmail.com>
2026-07-31 00:14:52 +0800

    fix(config): changed os.environ[] to os.environ.get() to avoid key errors in CI

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-PDF_PATH = os.environ["PDF_PATH"]
-TESS_DATA_PATH = os.environ["TESS_DATA_PATH"]
-RUNTIME_PARENT_DIR = os.environ["RUNTIME_PARENT_DIR"]
+PDF_PATH = os.environ.get("PDF_PATH")
+TESS_DATA_PATH = os.environ.get("TESS_DATA_PATH")
+RUNTIME_PARENT_DIR = os.environ.get("RUNTIME_PARENT_DIR")
 DEBUG_PDF_PATH = os.environ.get("DEBUG_PDF_PATH")
